408f88b71da4fe343a0012268ab8608fdf86ac59
Marko Jamelle <mjamelle@gmail.com>
2017-06-19 09:08:23 +0200

    add SQL DB user to DB connection

--- src/main/java/app/db/DB.java
+++ src/main/java/app/db/DB.java
@@ -3,5 +3,5 @@ package app.db;
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/onebank", null, null);
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/onebank", "postgres", "postgrespassword");
 }
