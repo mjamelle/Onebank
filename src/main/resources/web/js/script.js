74eae9deaf983eb9f1ebe1e093948abb051b03e0
Marko Jamelle <mjamelle@gmail.com>
2017-07-30 21:09:32 +0200

    change menu items. delete from handling and use JS jquery in order to aline web design

--- src/main/resources/web/js/script.js
+++ src/main/resources/web/js/script.js
@@ -1,4 +1,20 @@
 
 $(document).ready(function () {
-      $('[data-toggle="tooltip"]').tooltip();  
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // send locale choose to actual web page
+    $('#locale_de').click(function() {
+        $.post( "?locale=de");
+        location.reload();
+    });      
+    $('#locale_en').click(function() {
+        $.post( "?locale=en");
+        location.reload();
+    });
+      
+      // send logout request
+    $('#logout_request').click(function() {
+        $.post( "/logout");
+        location.reload();
+      });
 });
