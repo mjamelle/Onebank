23b0abeeb2a5b19c1f0bd8d48ed375cfd6a96063
Marko Jamelle <mjamelle@gmail.com>
2017-08-31 15:44:09 +0200

    delete MArkos Test

--- src/main/java/app/bot/BotLogic.java
+++ src/main/java/app/bot/BotLogic.java
@@ -13,7 +13,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-Markos Test with Git Branching 
 */
 package app.bot;
 
