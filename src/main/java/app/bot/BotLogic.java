9869672baa615b9294a59c861cf43cd60bf2aea7
Marko Jamelle <mjamelle@gmail.com>
2017-12-24 11:17:24 +0100

    typo

--- src/main/java/app/bot/BotLogic.java
+++ src/main/java/app/bot/BotLogic.java
@@ -58,7 +58,7 @@ public class BotLogic {
         
         ++botrequestcounter;  //counts the Bot requests
         
-        /*  ---old implementation now coverd by api.ai
+        /*  ---old implementation now covered by api.ai
         
         Message receive  = new Message(), response = new Message();
         
